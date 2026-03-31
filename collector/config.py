6e54d84da9b6719457e0dcc1078b01902e00cb53
Micah Holland <mholland@macmini.lan>
2026-03-31 17:54:04 -0500

    fix: publish downlinks to msh/2/json/mqtt/ topic
    Meshtastic JSON downlink requires a channel named 'mqtt' with downlink
    enabled. Publishing to the SpaceBabies channel topic was silently ignored.
    Added 'mqtt' channel (index 1) to UFN1 and updated publish topic.

--- collector/config.py
+++ collector/config.py
@@ -15,7 +15,7 @@ class Config:
         self.supabase_url = os.environ.get("SUPABASE_URL")
         self.supabase_service_key = os.environ.get("SUPABASE_SERVICE_KEY")
         self.supabase_anon_key = os.environ.get("SUPABASE_ANON_KEY")
-        self.mqtt_publish_topic = os.environ.get("MQTT_PUBLISH_TOPIC", "msh/2/json/SpaceBabies/")
+        self.mqtt_publish_topic = os.environ.get("MQTT_PUBLISH_TOPIC", "msh/2/json/mqtt/")
         self.gateway_node_id = os.environ.get("GATEWAY_NODE_ID", "")  # hex e.g. !02ee16c8
         self.network_id = os.environ.get("NETWORK_ID", "okc-crew")
 
